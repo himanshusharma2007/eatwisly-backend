816354bde1d68b2b82a5f8b4c207fab6476dc895
himanshusharma2007 <sharmahimanshu6478@gmail.com>
2025-06-06 22:37:56 +0530

    chnages in user routes

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const authController = require("../controllers/userAuthController");
 const { protect } = require("../middlewares/auth");
+const { uploadImage } = require("../controllers/ImageController");
+const { getScanHistory, getScanById, deleteScan } = require("../controllers/ScanController");
 
 // Public routes
 router.post("/register", authController.register);
@@ -14,9 +16,9 @@ router.get("/me", protect, authController.getProfile);
 router.put("/me", protect, authController.updateProfile);
 router.post("/logout", protect, authController.logout);
 // User routes
-router.post('/images/upload', uploadMiddleware, ImageController.uploadImage);
-router.get('/scans', protect, ScanController.getScanHistory);
-router.get('/scans/:id', protect, ScanController.getScanById);
-router.delete('/scans/:id', protect, ScanController.deleteScan);
+router.post('/images/upload', uploadMiddleware, uploadImage);
+router.get('/scans', protect, getScanHistory);
+router.get('/scans/:id', protect, getScanById);
+router.delete('/scans/:id', protect, deleteScan);
 
 module.exports = router;
